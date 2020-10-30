3a428938ed2a3b30f7d79d4994589b1ceb08745d
sheptianbagja <sheptianbagjautama@gmail.com>
2020-10-30 15:07:54 +0700

    Membuat API GET

--- routes/media.js
+++ routes/media.js
@@ -5,6 +5,22 @@ const base64Img = require('base64-img');
 
 const { Media } = require('../models');
 
+router.get('/', async(req, res) => {
+  const media = await Media.findAll({
+    attributes:['id', 'image']
+  });
+
+  const mapperMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  });
+
+  return res.json({
+    status:'success',
+    data:mapperMedia
+  })
+});
+
 router.post('/', function(req, res) {
   const image = req.body.image;
 
@@ -19,7 +35,7 @@ router.post('/', function(req, res) {
     }
 
     const filename = filepath.split("\\").pop().split("/").pop();
-    const media = await Media.create({ image:`image/${filename}` });
+    const media = await Media.create({ image:`images/${filename}` });
 
     return res.json({
       status:'success',
@@ -28,7 +44,7 @@ router.post('/', function(req, res) {
         image:`${req.get('host')}/images/${filename}`
       }
     });
-  })
+  });
 });
 
 module.exports = router;
